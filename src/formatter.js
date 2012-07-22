467ee390c0716d4a04fc2dddbcbbfdc42821c953
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-07-22 13:47:35 +0200

    Added tests for handling all basic json-types. Added tests for arrays. - This includes single-level and single level of nested array.
    Added the multiline reporter for easier reading the test-failures.

--- src/formatter.js
+++ src/formatter.js
@@ -1,15 +1,48 @@
 module.exports = format
 
-var util = require('util')
-
-function format(input) {
-	var obj = JSON.parse(input)
-	  , out = objectMapper(obj)
-
-	return out
+format.parse = function(str) {
+	return format(JSON.parse(str))
 }
 
-function objectMapper(obj) {
+var util = require('util')
+
+function format(obj, lvl) {
+	lvl = lvl || 0
+
+	if(typeof obj === 'string') {
+		return util.format('"%s"', obj)
+	} else if(typeof obj === 'number') {
+		return obj.toString()
+	} else if(typeof obj === 'boolean') {
+		return obj.toString()
+	} else if(obj === null) {
+		return 'null'
+	} else if(Array.isArray(obj)) {
+		return arrayMapper(obj, lvl)
+	}
+
+	return objectMapper(obj, lvl)
+}
+
+function arrayMapper(obj, lvl) {
+	var formattedArray = obj.map(mapper).join('\n')
+	  , ret = util.format('[%s\n', formattedArray)
+
+	function mapper(obj, idx) {
+		var value = format(obj, lvl + 1).trim()
+		 , line = ' ' + value
+
+		if(idx) {
+			line = indent(',' + line, lvl + 1)
+		}
+
+		return line
+	}
+
+	return ret + indent(']', lvl + 1)
+}
+
+function objectMapper(obj, lvl) {
 	return util.format
 		( '{%s\n}'
 		, Object.keys(obj)
@@ -18,4 +51,9 @@ function objectMapper(obj) {
 			})
 			.join('\n,')
 		)
+}
+
+function indent(str, lvl) {
+	var pad = new Array(lvl || 0).join('  ')
+	return pad + str
 }
--- test/unit/formatter.spec.js
+++ test/unit/formatter.spec.js
@@ -4,14 +4,85 @@ describe('unit/formatter.spec.js', function() {
 	describe('When root is an object', function() {
 		describe('containing a single key-value pair', function() {
 			it('should put it on two lines', function() {
-				var result = formatter('{"a":"b"}')
+				var result = formatter.parse('{"a":"b"}')
 				expect(result).to.equal('{ "a": "b"\n}')
 			})
 		})
 		describe('with no other characters', function() {
 			it('should put it on two lines', function() {
-				var result = formatter('{}')
+				var result = formatter.parse('{}')
 				expect(result).to.equal('{\n}')
+			})
+		})
+	})
+
+	describe('When root is an array', function() {
+		describe('with nested arrays', function() {
+			it('should format it according to spec', function() {
+				var result = formatter.parse('[1, [], ["2", true]]')
+				expect(result).to.equal(
+					  '[ 1\n'
+					+ ', [\n'
+					+ '  ]\n'
+					+ ', [ "2"\n'
+					+ '  , true\n'
+					+ '  ]\n'
+					+ ']'
+				)
+			})
+		})
+		describe('with a single level', function() {
+			it('should format it according to spec', function() {
+				var result = formatter.parse('[1,"2", true, null]')
+				expect(result).to.equal(
+					  '[ 1\n'
+					+ ', "2"\n'
+					+ ', true\n'
+					+ ', null\n'
+					+ ']'
+				)
+			})
+		})
+		describe('with no other characters', function() {
+			it('should put it on two lines', function() {
+				var result = formatter.parse('[]')
+				expect(result).to.equal('[\n]')
+			})
+		})
+	})
+
+	describe('When root is null', function() {
+		it('should put it on one line', function() {
+			var result = formatter.parse('null')
+			expect(result).to.equal('null')
+		})
+	})
+
+	describe('When root is a boolean', function() {
+		it('should put it on one line', function() {
+			var result = formatter.parse('true')
+			expect(result).to.equal('true')
+		})
+	})
+
+	describe('When root is a string', function() {
+		it('should put it on one line', function() {
+			var result = formatter.parse('\n"abc" \n ')
+			expect(result).to.equal('"abc"')
+		})
+	})
+
+	describe('When root is a number', function() {
+		describe('without decimals', function() {
+			it('should put it on one line', function() {
+				var result = formatter.parse('\n12 \n ')
+				expect(result).to.equal('12')
+			})
+		})
+		describe('with decimals', function() {
+			it('should put it on one line', function() {
+				var result = formatter.parse('\n12.34 \n ')
+				expect(result).to.equal('12.34')
 			})
 		})
 	})
